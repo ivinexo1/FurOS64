3bb004433ecbe0726d0a084a14d40ae8febbaa3f
Kravataf <159631603+Kravataf@users.noreply.github.com>
2026-02-23 13:50:06 +0100

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -8,4 +8,6 @@ mmd -i fat.img ::/EFI/BOOT
 mcopy -i fat.img ../build/kernel ::/
 mcopy -i fat.img ../build/efi/EFI_STUB.efi ::/
 mcopy -i fat.img ../build/efi/BOOTX64.efi ::/EFI/BOOT
-qemu-system-x86_64 -d int,mmu -no-reboot -no-shutdown -cpu qemu64 -bios OVMF.fd -drive file=fat.img,if=ide,format=raw
+# qemu-system-x86_64 -d int,mmu -no-reboot -no-shutdown -cpu qemu64 -bios OVMF.fd -drive file=fat.img,if=ide,format=raw
+# aka je pointa printovania ? + win11 sa posere z toho
+qemu-system-x86_64 -no-reboot -no-shutdown -cpu qemu64 -bios OVMF.fd -drive file=fat.img,if=ide,format=raw
