2a020a7cc1ddc6fbb337306dc47024500e48bb0c
Kravataf <159631603+Kravataf@users.noreply.github.com>
2026-08-09 20:10:37 +0200

    idt

--- include/Arch/Interrupts/idt.h
+++ include/Arch/Interrupts/idt.h
@@ -8,12 +8,11 @@ typedef struct {
 } __attribute__((packed)) idtr_t;
 
 typedef struct {
-  uint16_t offset_1; // offset bits 0..15
-  uint16_t selector; // a code segment selector in GDT or LDT
-  uint8_t
-      ist; // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-  uint8_t type_attributes; // gate type, dpl, and p fields
-  uint16_t offset_2;       // offset bits 16..31
-  uint32_t offset_3;       // offset bits 32..63
-  uint32_t zero;           // reserved
+  uint16_t offset_1;
+  uint16_t selector;
+  uint8_t  ist;
+  uint8_t  type_attributes;
+  uint16_t offset_2;
+  uint32_t offset_3;
+  uint32_t zero;
 } __attribute__((packed)) idt_t;
--- src/Arch/Interrupts/idt.c
+++ src/Arch/Interrupts/idt.c
@@ -4,13 +4,14 @@
 static idtr_t interrupt_table_reg;
 static idt_t interrupt_gate[IDT_ENTRIES];
 
-int SetIdtHandler(uint8_t *address, uint32_t entry) {
-  interrupt_gate[entry].offset_1 = (uint16_t)*address;
+int SetIdtHandler(uint64_t address, uint32_t entry) {
+  interrupt_gate[entry].offset_1 = (uint16_t)address;
   interrupt_gate[entry].selector = 0x10;
   interrupt_gate[entry].ist = 0;
   interrupt_gate[entry].type_attributes = 0x8e;
-  interrupt_gate[entry].offset_1 = (uint16_t)(*address >> 16);
-  interrupt_gate[entry].offset_3 = (uint32_t)(*address >> 32);
+  interrupt_gate[entry].offset_2 = (uint16_t)(address >> 16);
+  interrupt_gate[entry].offset_3 = (uint32_t)(address >> 32);
+  interrupt_gate[entry].zero = 0;
   return 0;
 }
 
