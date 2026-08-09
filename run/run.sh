38a146581d25a6c922db0ffaf529cf9dec1a10bb
Kravataf <159631603+Kravataf@users.noreply.github.com>
2026-08-09 18:58:37 +0200

    Update run.sh

--- run/run.sh
+++ run/run.sh
@@ -1,3 +1,4 @@
+# this runs inside /build/
 cd ..
 cmake --build build
 cd run
@@ -8,7 +9,6 @@ mmd -i fat.img ::/EFI/BOOT
 mcopy -i fat.img ../build/kernel ::/
 mcopy -i fat.img ../build/efi/EFI_STUB.efi ::/
 mcopy -i fat.img ../build/efi/BOOTX64.efi ::/EFI/BOOT
-# qemu-system-x86_64 -d int,mmu -no-reboot -no-shutdown -cpu qemu64 -bios OVMF.fd -drive file=fat.img,if=ide,format=raw
-# aka je pointa printovania ? + win11 sa posere z toho
-# debugging and logging of triple faults
-qemu-system-x86_64 -d int -cpu qemu64 -bios OVMF.fd -drive file=fat.img,if=ide,format=raw
+# /run/
+# for windows users, run this outside of wsl
+qemu-system-x86_64 -cpu qemu64 -bios OVMF.fd -drive file=fat.img,if=ide,format=raw
