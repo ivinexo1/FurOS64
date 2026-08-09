83e9a37b34e6f6b9e418d908306853d06fbf6bbe
Kravataf <159631603+Kravataf@users.noreply.github.com>
2026-08-09 21:05:16 +0200

    isr

--- include/Arch/Interrupts/isr.h
+++ include/Arch/Interrupts/isr.h
@@ -1,3 +1,83 @@
+#pragma once
+#include <stdint.h>
+
 #define ISR_ENTRIES 256
 
-typedef struct {} isr_t;
+typedef struct registers {
+    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
+    uint64_t rdi, rsi, rbp, rbx, rdx, rcx, rax;
+    uint64_t int_no, err_code;
+    uint64_t rip, cs, rflags, rsp, ss;
+} registers_t;
+
+typedef void (*isr_t)(registers_t *);
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+extern void isr32();
+extern void isr33();
+extern void isr34();
+extern void isr35();
+extern void isr36();
+extern void isr37();
+extern void isr38();
+extern void isr39();
+extern void isr40();
+extern void isr41();
+extern void isr42();
+extern void isr43();
+extern void isr44();
+extern void isr45();
+extern void isr46();
+extern void isr47();
+extern void isr48();
+extern void isr49();
+extern void isr50();
+extern void isr51();
+extern void isr52();
+extern void isr53();
+extern void isr54();
+extern void isr55();
+extern void isr56();
+extern void isr57();
+extern void isr58();
+extern void isr59();
+extern void isr60();
+extern void isr61();
+extern void isr62();
+extern void isr63();
+
+void isr_install(void);
+void isr_handler(registers_t *r);
+void irq_handler(registers_t *r);
+void register_interrupt_handler(uint8_t n, isr_t handler);
--- src/Arch/Interrupts/isr.c
+++ src/Arch/Interrupts/isr.c
@@ -1,3 +1,9 @@
 #include "Arch/Interrupts/isr.h"
+#include "Arch/Interrupts/idt.h"
+#include "Terminal/stdio.h"
 
 isr_t interrupt_handlers[ISR_ENTRIES];
+
+void register_interrupt_handler(uint8_t n, isr_t handler) {
+    interrupt_handlers[n] = handler;
+}
