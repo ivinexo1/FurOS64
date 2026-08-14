c652ad97e2a1bff12409e72eb1f6883c6b1b9232
ivinexo <ivinexoiot@gmail.com>
2026-08-14 14:49:22 +0200

    added io to header

--- include/Arch/in_out.h
+++ include/Arch/in_out.h
@@ -1,1 +1,11 @@
 #include <stdint.h>
+
+static inline void outb(uint16_t port, uint8_t val);
+static inline void outw(uint16_t port, uint16_t val);
+static inline void outd(uint16_t port, uint32_t val);
+static inline void outl(uint16_t port, uint64_t val);
+
+static inline uint8_t inb(uint16_t port);
+static inline uint16_t inw(uint16_t port);
+static inline uint32_t ind(uint16_t port);
+static inline uint64_t inl(uint16_t port);
